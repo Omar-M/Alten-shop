b3fa18cec5d0dc091fe0b195dc46b6d961d3d450
Omar Mahboubi <omarmahboubi@MacBook-Air-de-Omar.local>
2025-02-20 23:08:43 +0100

    add cart badge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from "@angular/common";
 import {
   Component,
 } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { SplitterModule } from 'primeng/splitter';
 import { ToolbarModule } from 'primeng/toolbar';
+import { CartService } from "./products/data-access/cart.service";
 import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component";
 
 @Component({
@@ -11,8 +13,17 @@ import { PanelMenuComponent } from "./shared/ui/panel-menu/panel-menu.component"
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
   standalone: true,
-  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent],
+  imports: [RouterModule, SplitterModule, ToolbarModule, PanelMenuComponent, CommonModule],
 })
 export class AppComponent {
   title = "ALTEN SHOP";
+  cartCount: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.getCartCount().subscribe(count => {
+      this.cartCount = count;
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./shared/features/home/home.component";
+import { CartComponent } from "./products/features/cart/cart.component";
 
 export const APP_ROUTES: Routes = [
   {
@@ -11,5 +12,9 @@ export const APP_ROUTES: Routes = [
     loadChildren: () =>
       import("./products/products.routes").then((m) => m.PRODUCTS_ROUTES)
   },
+  {
+		path: "cart",
+		component: CartComponent,
+	},
   { path: "", redirectTo: "home", pathMatch: "full" },
 ];
